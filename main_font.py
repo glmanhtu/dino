c93025962a235b5b7fedc9e9f1ee3c107073d40c
VU Manh Tu <glmanhtu@gmail.com>
2023-12-21 00:04:02 +0100

    Update

--- main_font.py
+++ main_font.py
@@ -54,7 +54,7 @@ def get_args_parser():
     # Model parameters
     parser.add_argument('--arch', default='vit_small', type=str,
         choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small'] \
-                + torchvision_archs + torch.hub.list("facebookresearch/xcit:main"),
+                + torchvision_archs,
         help="""Name of architecture to train. For quick experiments with ViTs,
         we recommend using vit_tiny or vit_small.""")
     parser.add_argument('--multiscale', default=False, type=utils.bool_flag)
